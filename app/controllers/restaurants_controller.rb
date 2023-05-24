ab31c3997d20b54907c92694520727e6999d9cdb
Kirstin Penzes <kpenzes17@gmail.com>
2023-05-24 12:23:09 -0400

    Added show action and view, working on new action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,19 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  private
+
+  def resto_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/new', to: 'restaurants#new'
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
 end
